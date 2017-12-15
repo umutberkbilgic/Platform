2cfbd9ed70c2561dcf8e0ba4622b53b4def96556
anilerken <anilerken@users.noreply.github.com>
2017-12-15 16:46:31 +0300

    Update LeaderBoardMenu.java

--- alpha/src/view/LeaderBoardMenu.java
+++ alpha/src/view/LeaderBoardMenu.java
@@ -3,54 +3,86 @@ package view;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import org.lwjgl.input.Mouse;
+import java.awt.Font;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class LeaderBoardMenu extends BasicGameState {
-	Image background;
-	Image title;
-	int height = 0;
-	int width = 0;
+	// Properties
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();//to get the height and width of the screen
+	private int screen_width = (int)screenSize.getWidth();//screen width assigned
+	private int screen_height = (int) screenSize.getHeight();//screen height assigned
+	private final int buttonwidth = (int)(screen_width / 4.92);
+	private final int buttonheight = (int)(screen_height / 16.6);
+	private Image background;
 	private Button localButton;
 	private Button globalButton;
-	public void switchView(){
-
-	}
+	String local_results;
+	String global_results;
+	String result_to_display;
+	private Button backButton;
+	
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		// TODO Auto-generated method stub
 		background = new Image("data/img/login_screen_4.jpg");//background screen is same
-		title = new Image("data/img/space_out_title.png");
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();//to get the height and width of the screen
-		width = (int)screenSize.getWidth();//screen width assigned
-		height = (int) screenSize.getHeight();//screen height assigned
-		localButton = new Button(width*(0.28), height*(0.575),width*(0.16), height*(0.09), "data/img/local.png", 1,0);//this image will change
-		globalButton = new Button(width*(0.28), height*(0.575),width*(0.16), height*(0.09), "data/img/global.png", 1,0);//this image will change
-
+		localButton = new Button(screen_width*(0.3), screen_height*(0.1), buttonwidth, buttonheight, "data/img/local.png", 1);
+		globalButton = new Button(screen_width*(0.3) + buttonwidth, screen_height*(0.1), buttonwidth, buttonheight, "data/img/global.png", 1);
+		backButton = new Button(screen_width*(0.1) , screen_height*(0.7), screen_width / 19.2, screen_height / 16.6, "data/img/back.png", 1);
+		local_results = "HEEEEY \nthis is \nlocal leaderboard here";
+		global_results = "Now \nTHIS IS \nglobal leaderboard";
+		result_to_display = local_results;
 	}
+	
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		// TODO Auto-generated method stub
-		g.drawImage(background.getScaledCopy(width,height),0,0);
-		g.drawImage(title,(float)((width - title.getWidth())/ 2), 50f, (float)((width - title.getWidth())/2 + title.getWidth()), (float)(50 + title.getHeight()), 0f, 0f,(float)(title.getWidth()), (float)(title.getHeight()));
-		g.drawImage(localButton.getImage(), (int) (width*(0.28)), (int) (height*0.400));//local leaderboard button drawed
-		g.drawImage(globalButton.getImage(), (int) (width*(0.28)), (int) (height*0.525));//global leaderboard button drawed
+		g.drawImage(background.getScaledCopy(screen_width,screen_height),0,0);
+		g.drawImage(localButton.getImage(), (int)localButton.getX(), (int)localButton.getY());//local leaderboard button drawed
+		g.drawImage(globalButton.getImage(), (int)globalButton.getX(), (int)globalButton.getY());//global leaderboard button drawed
+		g.drawImage(backButton.getImage(), (int)backButton.getX(), (int)backButton.getY());
+		g.setFont(new TrueTypeFont(new Font("Comic Sans MS", 0, 30), false));
+		drawString(g, result_to_display,(int)(screen_width*(0.3)),(int)(screen_height*(0.2)) );
 
 	}
+	
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int arg) throws SlickException {
-		// TODO Auto-generated method stub
-
+		Input input = gc.getInput();
+		double xpos = Mouse.getX();
+		double ypos = screen_height - Mouse.getY(); // since for graphic elements y0 is at top, for mouse y0 is at bottom
+		// Check for button click and display proper string. global list or local list.
+		if(input.isMousePressed(0)) {
+			if(localButton.isPositionContained(xpos, ypos))
+				result_to_display = local_results; 
+			else if(globalButton.isPositionContained(xpos, ypos))
+				result_to_display = global_results; 
+			else if(backButton.isPositionContained(xpos, ypos))
+				sbg.enterState(1);
+		}
 	}
+	
+	@Override
+	public void keyPressed(int key, char code){
+		if(key == Input.KEY_ESCAPE)
+			System.exit(0);
+	}
+	
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 2;
+	}
+	
+	// thanks to https://stackoverflow.com/questions/4413132/problems-with-newline-in-graphics2d-drawstring
+	void drawString(Graphics g, String text, int x, int y) {
+	    for (String line : text.split("\n"))
+	        g.drawString(line, x, y += g.getFont().getLineHeight());
 	}
 
 }
