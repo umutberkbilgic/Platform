84b2c91341cfc97d82b0dca03514683276a8dfbc
anilerken <anilerken@users.noreply.github.com>
2017-12-15 23:42:26 +0300

    Update SettingsMenu.java

--- alpha/src/view/SettingsMenu.java
+++ alpha/src/view/SettingsMenu.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Toolkit;
 
 import org.lwjgl.input.Mouse;
@@ -9,6 +10,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.gui.TextField;
@@ -26,31 +28,17 @@ public class SettingsMenu  extends BasicGameState{
 	private Button changeSkinButton;
 	private Button changePasswordButton;
 	private Button changeUsernameButton;
-	private Button[] skinsList;
-	private Button changeButton;
-	private Image currentpass;
-	private Image newpass;
-	private Image newpassagain;
-	private Image desiredname;
-	private TextField currentpassfield;
-	private TextField newpassfield;
-	private TextField newpassagainfield;
-	private TextField desirednamefield;
-	private boolean skins_switch;
-	private boolean pass_switch;
-	private boolean usrname_switch;
 	private Button backButton;
 	
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		skins_switch = pass_switch = usrname_switch = false;
 		background = new Image("data/img/login_screen_4.jpg");//background screen is same
 		title = new Image("data/img/space_out_title.png");
 		changeSkinButton = new Button(screen_width*(0.1), screen_height*(0.3),buttonwidth, buttonheight, "data/img/change_skin.png", 1);
 		changePasswordButton = new Button(screen_width*(0.1), screen_height*(0.4),buttonwidth, buttonheight, "data/img/change_password.png", 1);
 		changeUsernameButton = new Button(screen_width*(0.1), screen_height*(0.5),buttonwidth, buttonheight, "data/img/change_username.png", 1);
 		backButton = new Button(screen_width*(0.1) , screen_height*(0.7), screen_width / 19.2, screen_height / 16.6, "data/img/back.png", 1);
-	}
+		}
 	
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
@@ -60,7 +48,6 @@ public class SettingsMenu  extends BasicGameState{
 		g.drawImage(changePasswordButton.getImage(), (int)changePasswordButton.getX(), (int)changePasswordButton.getY());
 		g.drawImage(changeUsernameButton.getImage(), (int)changeUsernameButton.getX(), (int)changeUsernameButton.getY());
 		g.drawImage(backButton.getImage(), (int)backButton.getX(), (int)backButton.getY());
-		//renderThings(g, skins_switch, pass_switch, usrname_switch);
 	}
 	
 	@Override
@@ -70,8 +57,18 @@ public class SettingsMenu  extends BasicGameState{
 		double ypos = screen_height - Mouse.getY(); // since for graphic elements y0 is at top, for mouse y0 is at bottom
 		// Check for button click and navigate to next state
 		if(input.isMousePressed(0)) {
-			if(backButton.isPositionContained(xpos, ypos))
+			if(backButton.isPositionContained(xpos, ypos)) {
 				sbg.enterState(1);
+			}
+			else if(changePasswordButton.isPositionContained(xpos, ypos)) {
+				sbg.enterState(8);
+			}
+			else if(changeUsernameButton.isPositionContained(xpos, ypos)) {
+				sbg.enterState(9);
+			}
+			else if(changeSkinButton.isPositionContained(xpos, ypos)) {
+				
+			}	
 		}	
 	}
 	
@@ -79,15 +76,6 @@ public class SettingsMenu  extends BasicGameState{
 	public void keyPressed(int key, char code){
 		if(key == Input.KEY_ESCAPE)
 			System.exit(0);
-	}
-	
-	public void renderThings(Graphics g, boolean skins_switch, boolean pass_switch, boolean usrname_switch ) {
-		if( skins_switch) {
-			// there be skin buttons and change button
-		}
-		else if( pass_switch) {
-		}
-		else if(usrname_switch) {}
 	}
 	
 	@Override
